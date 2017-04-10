03f5095f0588e65a332b5a931565ae0a3c16ea12
weaming <garden.yuen@gmail.com>
2017-04-10 17:52:38 +0800

    fix: import error

--- googletrans/__init__.py
+++ googletrans/__init__.py
@@ -4,5 +4,14 @@ __version_info__ = 2, 1, 2
 __version__ = '.'.join(str(v) for v in __version_info__)
 
 
+import os
 from googletrans.client import Translator
 from googletrans.constants import LANGUAGES
+
+config_path = os.path.expanduser('~/.googletrans')
+if not os.path.isfile(config_path):
+    with open(config_path, 'w') as f:
+        f.write('translate.google.com')
+
+with open(config_path, 'r') as f:
+    translator = Translator(map(lambda x: x.strip(), list(f)))
